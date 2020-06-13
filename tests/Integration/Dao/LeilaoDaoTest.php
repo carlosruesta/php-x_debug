5b0a73905fd92f64f6580122eae2451ce204e939
Carlos Ruesta <carlos_ruesta@yahoo.es>
2020-06-13 17:14:55 -0300

    adicionando um teste novo

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -42,6 +42,8 @@ class LeilaoDaoTest extends TestCase
         // act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        var_dump($leiloes);
+
         // assert
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
